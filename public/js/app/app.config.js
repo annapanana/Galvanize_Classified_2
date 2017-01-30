df28956bc944f07bf6347d5f6b9aa97c3b6db595
Anna Lotko <annaklotko@gmail.com>
2017-01-30 10:59:15 -0700

    angular configured with UI router setting the user at the home state

--- public/js/app/app.config.js
+++ public/js/app/app.config.js
@@ -1,12 +1,12 @@
 'use strict';
 (function() {
+
   angular.module('app')
     .config(config)
 
     config.$inject = ["$stateProvider", "$urlRouterProvider", "$locationProvider"];
     function config($stateProvider, $urlRouterProvider, $locationProvider) {
       $locationProvider.html5Mode(true);
-
       $stateProvider
         .state({
           name: "app",
