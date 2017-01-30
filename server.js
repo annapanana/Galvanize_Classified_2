aab1ebf601eeaea269a17e341d00e9b563e3b951
Anna Lotko <annaklotko@gmail.com>
2017-01-30 10:42:42 -0700

    set up angular dependencies and folder structure

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(bodyParser.json());
 app.use(express.static('./public'));
 app.use('/classifieds',messages);
 
+app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist/js'));
+app.use('/angular', express.static('node_modules/angular'));
+app.use('/ui-bootstrap', express.static('node_modules/ui-bootstrap/'));
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
