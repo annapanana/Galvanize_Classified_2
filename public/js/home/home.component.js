24dde4278dc4a7b8caece03b6339c166fda5a981
Anna Lotko <annaklotko@gmail.com>
2017-01-30 11:21:01 -0700

    post request working from angular form

--- public/js/home/home.component.js
+++ public/js/home/home.component.js
@@ -9,11 +9,20 @@
     controller.$inject = ["$http", "$state"]
     function controller($http, $state) {
       const vm = this;
+      vm.isPosting = true;
+      vm.newPost = {};
       vm.$onInit = function() {
         // Pull all adds
         $http.get('/classifieds').then((result) => {
-          console.log(result.data);
+          // console.log(result.data);
           vm.posts = result.data;
+        });
+      };
+
+      vm.addPost = function() {
+        // console.log("addPost", vm.newPost);
+        $http.post('/classifieds', vm.newPost).then((result) => {
+          console.log(result);
         });
       };
     }
